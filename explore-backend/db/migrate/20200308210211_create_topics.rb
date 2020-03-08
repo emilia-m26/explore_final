30004cb681c8f7b49125c51b74a9691dc670217f
Emilia <emilia.mena26@gmail.com>
2020-03-08 17:03:16 -0400

    topic model, controller, migration, added foreign key - not migrated yet

---
+++ explore-backend/db/migrate/20200308210211_create_topics.rb
@@ -0,0 +1,11 @@
+class CreateTopics < ActiveRecord::Migration[6.0]
+  def change
+    create_table :topics do |t|
+      t.string :name
+      t.string :vid_url
+      t.integer : country_id
+
+      t.timestamps
+    end
+  end
+end
