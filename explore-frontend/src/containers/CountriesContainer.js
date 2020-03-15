0c4fc6104bac2bd8e1290ea71bb52c4df519cb0c
Emilia <emilia.mena26@gmail.com>
2020-03-15 15:34:10 -0400

    adding imports to countries container

--- explore-frontend/src/containers/CountriesContainer.js
+++ explore-frontend/src/containers/CountriesContainer.js
@@ -1,4 +1,7 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import {fetchCountries} from '../actions/fetchCountries';
+import Countries from '../components/Countries';
 
 class CountriesContainer extends Component {
     render () {
