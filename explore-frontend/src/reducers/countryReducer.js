d286a3792afee2d1ea8b1044c45c5e5ed355dad4
Emilia <emilia.mena26@gmail.com>
2020-03-15 15:32:20 -0400

    working on fetching countries

--- explore-frontend/src/reducers/countryReducer.js
+++ explore-frontend/src/reducers/countryReducer.js
@@ -2,6 +2,8 @@ export default function countryReducer (state = { countries:[] }, action) {
    
     switch (action.type){
         //need to add fetchCountries to functionality
+        case 'FETCH_COUNTRIES':
+            return  {...state, countries: action.payload}  
         case 'ADD_EMAIL':
             return {...state, emails: action.payload }
         default:
