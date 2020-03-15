40350d4920e1b2b2d0d5f7947f55c5507ecde81a
Emilia <emilia.mena26@gmail.com>
2020-03-15 14:44:28 -0400

    working on adding store, not complete yet

--- explore-frontend/src/index.js
+++ explore-frontend/src/index.js
@@ -6,10 +6,20 @@ import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
 import {createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
+import countryReducer from './reducers/countryReducer.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const store = createStore(
+    countryReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+ReactDOM.render(
+<Provider store={store}>
+   <App />
+</Provider>, 
+document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
