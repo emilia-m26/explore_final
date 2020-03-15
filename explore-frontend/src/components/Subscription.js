76a343cf417420b41c766f8868ecd8e78ad9a9d6
Emilia <emilia.mena26@gmail.com>
2020-03-15 14:31:35 -0400

    working on subscription functionality

--- explore-frontend/src/components/Subscription.js
+++ explore-frontend/src/components/Subscription.js
@@ -8,14 +8,30 @@ class Subscription extends Component {
     //add name later once I update migration
     state = {
         email:''
+        //name: ''
+    }
+
+
+    handleChange = (event) => {
+        this.setState({
+            email: event.target.value
+        });
+    }
+
+    handleSubmit =(event) => {
+      event.preventDefault()
+      this.props.addEmail(this.state)
+      this.setState({
+          email:''
+      })
     }
     render () {
         return (
             <div className="container text-center">
-            <Form>
+            <Form onSubmit={this.handleSubmit}>
                 <Form.Group controlId="formBasicEmail">
                 <Form.Label>Email Address</Form.Label>
-                <Form.Control type="email" placeholder="Enter email" />
+                <Form.Control type="email" placeholder="email@domain.com" name="email" value={this.state.email} onChange={this.handleChange}/>
                 <Form.Text className="text-muted">
                 We'll never share your email with anyone else.
                 </Form.Text>
