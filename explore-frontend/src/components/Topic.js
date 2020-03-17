370b0bb271fd1f1486a0905da172ca217d2395cc
Emilia <emilia.mena26@gmail.com>
2020-03-17 19:41:15 -0400

    working on topics functionality, topics.js

--- explore-frontend/src/components/Topic.js
+++ explore-frontend/src/components/Topic.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Badge from 'react-bootstrap/Badge';
+import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 
-export const Topic = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+const Topic = ({topic}) => {
+
+    console.log(topic)
+
+    return(
+
+    <div>
+          <h2>{topic.name}</h2>
+          <h4>{topic.description}</h4> 
+          <h4>
+            <Button variant="info" as={Link} to='/countries'>Return to Countries</Button>
+          </h4>
+        </div>         
+  );
 }
+  
+  export default Topic;
+
+  //<Badge pill variant="info" as={Link} to='/countries'>Return to Countries</Badge>
