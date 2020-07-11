b7e71acf15a0370b84c7cd6b1dc142658500cdc5
Emilia <emilia.mena26@gmail.com>
2020-07-11 18:56:33 -0400

    changes to ImgComp

--- explore-frontend/src/components/ImgComp.js
+++ explore-frontend/src/components/ImgComp.js
@@ -2,11 +2,15 @@ import React from "react";
 import "../slider.scss";
 
 const ImgComp = ({ src }) => {
-  let imgStyles = {
-    width: 100 + "%",
-    height: "auto"
-  };
-  return <img src={src} alt="slide-img" style={imgStyles}></img>;
+  // let imgStyles = {
+  //   width: 80 + "%",
+  //   height: 100 + "%",
+  //   margin: "auto"
+
+  //style={imgStyles}
+   
+  // };
+  return <img className="center" src={src} alt="slide-img" ></img>;
 };
 
 export default ImgComp;
