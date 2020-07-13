e667d3c7fac2bea58641e8bb4b84f3126d5940da
Emilia <emilia.mena26@gmail.com>
2020-07-13 17:07:38 -0400

    added countries 11-15

--- explore-backend/db/seeds.rb
+++ explore-backend/db/seeds.rb
@@ -5,32 +5,37 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#countries 1-5
 Country.create(name:"Japan", detail:"Home of the Sakura (Cherry Blossom) trees", img_url:"https://lp-cms-production.imgix.net/2019-06/d29f08e97892053daeecc25ff5de908a-japan.jpeg")
 Country.create(name:"Brazil", detail:"Country of Samba and Futebol", img_url:"https://www.arcgis.com/sharing/rest/content/items/83eb9c53424a40808ac4fc3b99e60e38/resources/1572007732367.jpeg?w=2774")
 Country.create(name:"Puerto Rico", detail:"La Isla del Encanto", img_url:"https://media.architecturaldigest.com/photos/5ace1d482204b06f8e237dd6/16:9/w_2560%2Cc_limit/GettyImages-637492668.jpg")
 Country.create(name:"Egypt",detail:"Home to the Pyramids and the Sphinx", img_url: "https://assets3.thrillist.com/v1/image/2848962/size/gn-gift_guide_variable_c.jpg")
 Country.create(name:"India",detail:"Home to one of the Seven Wonders of the World: the Taj Mahal", img_url: "https://www.businessdestinations.com/wp-content/uploads/2014/04/Taj-Mahal.jpg")
-
+#countries 6-10
 Country.create(name:"Hawaii",detail:"Home of Aloha", img_url: "https://www.gohawaii.com/sites/default/files/styles/image_gallery_bg_xl/public/hero-unit-images/12709.jpg?itok=RByMHmpc")
 Country.create(name:"Greece",detail:"Creators of the Olympic Games", img_url: "ttps://www.scotsman.com/webimg/b25lY21zOjEwNTE0NTg2LWUxM2MtNDAxNi04N2JhLTBkODQ2MGY5MjQ3ZDozZGNmMmI4ZS02NDU3LTRkOTgtYjZiNC04MjkwYjg1NWU0YWI=.jpg?width=640")
 Country.create(name:"Italy",detail:"Home to the Sistine Chapel", img_url: "https://cdn.britannica.com/82/195482-050-2373E635/Amalfi-Italy.jpg")
 Country.create(name:"Cabo Verde",detail:"An island nation on an archipelago of volcanic origin in the Atlantic Ocean", img_url: "https://blogs.afdb.org/sites/default/files/blog-caboverde-1.jpg")
 Country.create(name:"Zimbabwe",detail:"Country of Victoria Falls, one of the biggest waterfalls in the world", img_url: "https://tullyluxurytravel.com/wp-content/uploads/2019/11/zimbabwe-aspect-ratio-2500x1400.jpg")
+#countries 11-15
+Country.create(name:"South Africa",detail:"", img_url: "https://www.southafrica.com/wp-content/uploads/2020/01/Canva-Cape-Town-South-Africa-aerial-view.jpg")
+Country.create(name:"Samoa",detail:"", img_url: "https://media.worldnomads.com/travel-safety/samoa/samoalead-SouthernLightscapesAustralia.jpg")
+Country.create(name:"New Zealand (Aotearoa)",detail:"", img_url: " https://www.newzealand.com/assets/Tourism-NZ/Fiordland/img-1536137761-110-7749-p-7ECF7092-95BD-FE18-6D4107E2E42D067E-2544003__aWxvdmVrZWxseQo_FocalPointCropWzQyNyw2NDAsNTAsNTAsODUsImpwZyIsNjUsMi41XQ.jpg")
+Country.create(name:"Tonga",detail:"", img_url: "https://www.beltandroad.news/wp-content/uploads/2019/08/Tonga-and-Pacific-Islands.jpg")
+Country.create(name:"Indonesia",detail:"", img_url: "https://img.jakpost.net/c/2016/09/21/2016_09_21_12422_1474449430._large.jpg")
+#countries 16-20
 Country.create(name:"",detail:"", img_url: "")
 Country.create(name:"",detail:"", img_url: "")
 Country.create(name:"",detail:"", img_url: "")
 Country.create(name:"",detail:"", img_url: "")
 Country.create(name:"",detail:"", img_url: "")
+#countries 21-25
 Country.create(name:"",detail:"", img_url: "")
 Country.create(name:"",detail:"", img_url: "")
 Country.create(name:"",detail:"", img_url: "")
 Country.create(name:"",detail:"", img_url: "")
 Country.create(name:"",detail:"", img_url: "")
-Country.create(name:"",detail:"", img_url: "")
-Country.create(name:"",detail:"", img_url: "")
-Country.create(name:"",detail:"", img_url: "")
-Country.create(name:"",detail:"", img_url: "")
-Country.create(name:"",detail:"", img_url: "")
+#countries 26-30
 Country.create(name:"",detail:"", img_url: "")
 Country.create(name:"",detail:"", img_url: "")
 Country.create(name:"",detail:"", img_url: "")
@@ -97,35 +102,35 @@ Topic.create(country_id:10, name: "Art", description:"Art of Zimbabwe", vid_url:
 Topic.create(country_id:10, name: "Dance", description:"Traditional Dances of Zimbabwe", vid_url:"https://www.youtube.com/watch?v=ALErR9ossP0")
 Topic.create(country_id:10, name: "Food", description:"Food of Zimbabwe", vid_url:"ttps://www.youtube.com/watch?v=OSvZ-7hxmrY")
 
-#country 11 topics - 
-Topic.create(country_id:11, name: "Language", description:"", vid_url:"")
-Topic.create(country_id:11, name: "Art", description:"Art of ", vid_url:"")
-Topic.create(country_id:11, name: "Dance", description:"Traditional Dances of ", vid_url:"")
-Topic.create(country_id:11, name: "Food", description:"Street Food of ", vid_url:"")
+#country 11 topics - south africa
+Topic.create(country_id:11, name: "Language", description:"The Xhosa Lanuage", vid_url:"https://www.youtube.com/watch?v=sGMwOAYOxhE")
+Topic.create(country_id:11, name: "Art", description:"Art of South Africa", vid_url:"https://www.youtube.com/watch?v=fAkIYS5We4c")
+Topic.create(country_id:11, name: "Dance", description:"Dances of South Africa", vid_url:"https://www.youtube.com/watch?v=wcajK5farZ0")
+Topic.create(country_id:11, name: "Food", description:"Street Food of South Africa ", vid_url:"https://www.youtube.com/watch?v=A5DDth0FOIo")
 
-#country 12 topics - 
-Topic.create(country_id:12, name: "Language", description:"", vid_url:"")
-Topic.create(country_id:12, name: "Art", description:"Art of ", vid_url:"")
-Topic.create(country_id:12, name: "Dance", description:"Traditional Dances of ", vid_url:"")
-Topic.create(country_id:12, name: "Food", description:"Street Food of ", vid_url:"")
+#country 12 topics - samoa
+Topic.create(country_id:12, name: "Language", description:"The Samoan Language", vid_url:"https://www.youtube.com/watch?v=Hgl8ANScl6o")
+Topic.create(country_id:12, name: "Art", description:"Art of Samoa", vid_url:"https://www.youtube.com/watch?v=97KtKqMpbd0")
+Topic.create(country_id:12, name: "Dance", description:"Traditional Dances of Samoa", vid_url:"https://www.youtube.com/watch?v=VrHtTOLRm1c")
+Topic.create(country_id:12, name: "Food", description:"Food of Samoa", vid_url:"ps://www.youtube.com/watch?v=8HIhSQuOWbA")
 
-#country 13 topics - 
-Topic.create(country_id:13, name: "Language", description:"", vid_url:"")
-Topic.create(country_id:13, name: "Art", description:"Art of ", vid_url:"")
-Topic.create(country_id:13, name: "Dance", description:"Traditional Dances of ", vid_url:"")
-Topic.create(country_id:13, name: "Food", description:"Street Food of ", vid_url:"")
+#country 13 topics - new zealand(aotearoa)
+Topic.create(country_id:13, name: "Language", description:"Maori - the native language of Aotearoa (New Zealand)", vid_url:"https://www.youtube.com/watch?v=vEqkoTIW5yI")
+Topic.create(country_id:13, name: "Art", description:"Art of New Zealand", vid_url:"https://www.youtube.com/watch?v=PGWgivKiKng")
+Topic.create(country_id:13, name: "Dance", description:"Dances of New Zealand", vid_url:"https://www.youtube.com/watch?v=G_OMxvhc358")
+Topic.create(country_id:13, name: "Food", description:"Food of New Zealand", vid_url:"https://www.youtube.com/watch?v=nCbhL4Ed0RE")
 
-#country 14 topics - 
-Topic.create(country_id:14, name: "Language", description:"", vid_url:"")
-Topic.create(country_id:14, name: "Art", description:"Art of ", vid_url:"")
-Topic.create(country_id:14, name: "Dance", description:"Traditional Dances of ", vid_url:"")
-Topic.create(country_id:14, name: "Food", description:"Street Food of ", vid_url:"")
+#country 14 topics - tonga
+Topic.create(country_id:14, name: "Language", description:"The Tongan Language", vid_url:"https://www.youtube.com/watch?v=uf2RDTplWs4")
+Topic.create(country_id:14, name: "Art", description:"Art of Tonga", vid_url:"https://www.youtube.com/watch?v=44QBPhj2zcY")
+Topic.create(country_id:14, name: "Dance", description:"Traditional Dances of Tonga", vid_url:"https://www.youtube.com/watch?v=GVffVH2WLbk")
+Topic.create(country_id:14, name: "Food", description:"Food of Tonga", vid_url:"https://www.youtube.com/watch?v=VRe40xasT98")
 
-#country 15 topics - 
-Topic.create(country_id:15, name: "Language", description:"", vid_url:"")
-Topic.create(country_id:15, name: "Art", description:"Art of ", vid_url:"")
-Topic.create(country_id:15, name: "Dance", description:"Traditional Dances of ", vid_url:"")
-Topic.create(country_id:15, name: "Food", description:"Street Food of ", vid_url:"")
+#country 15 topics - indonesia
+Topic.create(country_id:15, name: "Language", description:"The Indonesian Language", vid_url:"https://www.youtube.com/watch?v=usVkjWzaScc")
+Topic.create(country_id:15, name: "Art", description:"Art of Indonesia ", vid_url:"https://www.youtube.com/watch?v=AJoOlCRgm84")
+Topic.create(country_id:15, name: "Dance", description:"Traditional Dances of Indonesia", vid_url:"https://www.youtube.com/watch?v=k9lNc4fu8Zc")
+Topic.create(country_id:15, name: "Food", description:"Food of Indonesia ", vid_url:"https://www.youtube.com/watch?v=9GLwZ_3bNSY")
 
 #country 16 topics - 
 Topic.create(country_id:16, name: "Language", description:"", vid_url:"")
